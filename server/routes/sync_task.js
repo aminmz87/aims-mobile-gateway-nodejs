7c3f89921bae8437d38ff41c8ad510e14a058f69
Mohd Norul Amin <aminmz87@gmail.com>
2016-11-23 11:13:35 +0800

    app

--- server/routes/sync_task.js
+++ server/routes/sync_task.js
@@ -10,11 +10,11 @@ module.exports = (app, mysqlAsset, couchAsset) => {
     const uuid = req.query.uuid;
 
     autoInject({
-      checkDbInspTask: function (cb) {
+      checkDbInspTask: (cb) => {
         couchInspTask.exists(cb);
       },
 
-      prepDbInspTask: function (checkDbInspTask, cb) {
+      prepDbInspTask: (checkDbInspTask, cb) => {
         if (!checkDbInspTask) {
           couchInspTask.create(cb);
         } else {
@@ -22,11 +22,11 @@ module.exports = (app, mysqlAsset, couchAsset) => {
         }
       },
 
-      checkDbInspTaskItem: function (cb) {
+      checkDbInspTaskItem: (cb) => {
         couchInspTaskItem.exists(cb);
       },
 
-      prepDbInspTaskItem: function (checkDbInspTaskItem, cb) {
+      prepDbInspTaskItem: (checkDbInspTaskItem, cb) => {
         if (!checkDbInspTaskItem) {
           couchInspTaskItem.create(cb);
         } else {
@@ -34,13 +34,31 @@ module.exports = (app, mysqlAsset, couchAsset) => {
         }
       },
 
-      createDbInspTaskItemFilter: function (prepDbInspTaskItem, cb) {
+      prepDbInspTaskItemFilter: (checkDbInspTaskItem, prepDbInspTaskItem, cb) => {
+        if (!checkDbInspTaskItem) {
+          console.log('create filter');
+          couchInspTaskItem.save('_design/asset_insp_task_item', {
+            views: {
+              all: {
+                map: 'function (doc) { emit([doc.Ref_No], doc) }'
+              }
+            }
+          }, cb);
+
+        } else {
+          cb();
+
+        }
+
+      },
+
+      createDbInspTaskItemFilter: (prepDbInspTaskItem, cb) => {
         //todo filter was not supported in cradle.
 
       },
 
-      getUserId: function (cb) {
-        couchMapUser.get(uuid, function (err, docs) {
+      getUserId: (cb) => {
+        couchMapUser.get(uuid, (err, docs) => {
           if(err) {
             cb(err);
           } else {
@@ -49,35 +67,35 @@ module.exports = (app, mysqlAsset, couchAsset) => {
         });
       },
 
-      retrieveLegacyInspTask: function (getUserId, cb) {
+      retrieveLegacyInspTask: (getUserId, cb) => {
         mysqlAsset.raw('SELECT i.ID, i.Title, i.Ref_no, i.Creation_date, c.Description FROM insp_task i INNER JOIN code_001_cawangan c ON (Left(i.daerah_pemeriksaan,6) = CONCAT(c.Code_Negeri, c.Code_Daerah, c.Code)) WHERE i.Status = "A" AND userid = ' + getUserId.user_id + ' OR Officer2_uid=' + getUserId.user_id).asCallback(cb);
       },
 
-      saveInspTask: function (prepDbInspTask, retrieveLegacyInspTask, cb) {
-        var refnos = [];
+      saveInspTask: (prepDbInspTask, retrieveLegacyInspTask, cb) => {
+        let refnos = [];
 
-        each(retrieveLegacyInspTask[0], function (task, cb) {
+        each(retrieveLegacyInspTask[0], (task, cb) => {
           refnos.push(task.Ref_no);
           couchInspTask.save(task.Ref_no.toString(), task, cb);
 
-        }, function (err) {
+        }, (err) => {
             cb(err, refnos);
         });
       },
 
-      retrieveLegacyInspTaskItem: function (prepDbInspTaskItem, getUserId, saveInspTask, cb) {
+      retrieveLegacyInspTaskItem: (prepDbInspTaskItem, getUserId, saveInspTask, cb) => {
         const refnos = saveInspTask;
-        var stringRefnos = refnos.length === 0 ? '' : '"' + refnos.join('","') + '"';
+        let stringRefnos = refnos.length === 0 ? '' : '"' + refnos.join('","') + '"';
 
-        mysqlAsset.raw('SELECT ID, Ref_No, Asset_DB_ID, Code, kategori, aset, aset_daerah, Curr_Location, Curr_LocCode, Curr_Location_Description, prev_state, Creation_Date, Prev_Officer_DB_ID, Prev_Officer_ID, Prev_Officer, Prev_Officer_Designation, Prev_Officer_Daerah, Curr_Officer, Curr_Officer_DB_ID, Curr_Officer_ID, Curr_Officer_Designation, Curr_Officer_Daerah, rec_date, brand, JenisHarta, label, cost, chasis, reg_serial, Exact_Location, Exact_LocCode, Exact_Location_Description, Verify_Move, State, catatan, PA2_Lengkap, PA2_Kemaskini FROM 312_inspection WHERE Ref_No IN (' + stringRefnos + ')').asCallback(function (err, items) {
+        mysqlAsset.raw('SELECT ID, Ref_No, Asset_DB_ID, Code, kategori, aset, aset_daerah, Curr_Location, Curr_LocCode, Curr_Location_Description, prev_state, Creation_Date, Prev_Officer_DB_ID, Prev_Officer_ID, Prev_Officer, Prev_Officer_Designation, Prev_Officer_Daerah, Curr_Officer, Curr_Officer_DB_ID, Curr_Officer_ID, Curr_Officer_Designation, Curr_Officer_Daerah, rec_date, brand, JenisHarta, label, cost, chasis, reg_serial, Exact_Location, Exact_LocCode, Exact_Location_Description, Verify_Move, State, catatan, PA2_Lengkap, PA2_Kemaskini FROM 312_inspection WHERE Ref_No IN (' + stringRefnos + ')').asCallback((err, items) => {
           if(err) {
             cb(err);
           }
 
-          each(items[0], function (item, cb) {
+          each(items[0], (item, cb) => {
             couchInspTaskItem.save(item, cb);
 
-          }, function (err) {
+          }, (err) => {
             if(err) {
               cb(err);
 
